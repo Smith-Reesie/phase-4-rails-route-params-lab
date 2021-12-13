51f1fa01326d6f93340025b6d9bbbc31fe16e638
Maurice Smith <Xmaurice.smith@gmail.com>
2021-12-13 16:07:36 -0500

    commit tree 4fa8eb6cbdc3f3f017d9601019ff2cc0f2a690d3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,52 @@
 class StudentsController < ApplicationController
 
+
   def index
-    students = Student.all
+    students = if params[:name]
+        Student.by_name(params[:name])
+      else
+        Student.all
+      end    
     render json: students
   end
+
+  
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  
+  ## CRUD actions vv
+  # before_actions :find_student, only: [:show, :update, :destroy]
+
+  # def index
+  #   students = Student.all
+  #   render json: students
+  # end
+
+  # def show 
+  #   render json: @student
+  # end 
+
+  # def create 
+  #   student = Student.create!(params)
+  #   render json: student
+  # end
+
+  # def update 
+  #   @student.update(params)
+  #   render json: @student
+  # end 
+
+  # def destroy  
+  #   @student.destroy
+  #   render json: @student
+  # end 
+
+  # private
+
+  # def find_student 
+  #   @student = Student.find(params[:id])
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/students', to: 'students#index'
+  get '/students', to: 'students#index' #READ ONE 
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/:id' to: 'students#show' # READ ONE 
+  # post '/students' to: 'students#create' # CREATE
+  # put '/students' to: 'students#update' #UPDATE
+  # delete '/students' to: 'students#' #DELETE
 end
